6bc3fe1aacaf883c7611d523b05a5feb62aa970a
Jacob Garland <352cjg@gmail.com>
2024-11-26 22:52:22 -0500

    index file built

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { initDB } from "./database/db";
 import { mainMenu } from "./application/prompts";
 
+const runApp = async () => {
+    console.log('Starting Employee Management System...');
+    try {
+        await initDB();
+        await mainMenu();
+    } catch (err) {
+        console.error('Error occured starting the application...', err);
+    } finally {
+        console.log('Exiting EMS. Goodbye!');
+        process.exit();
+    }
+};
+
+runApp();
